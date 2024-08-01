e35b12cd44a1a5c3410457ddd21ff16f8acd5a3c
anaisdufourneau <anais.dufourneau87@gmail.com>
2024-08-01 10:44:03 +0200

    harmonia-3 done!

--- server/index.js
+++ server/index.js
@@ -9,10 +9,14 @@ require("./database/client").checkConnection();
 // Import the Express application from app/config.js
 const app = require("./app/config");
 
-const wildSeries = "Welcome to Wild Series !";
-app.get("/", (req, res) => {
-  res.send(wildSeries);
-});
+// const wildSeries = "Welcome to Wild Series !";
+// app.get("/", (req, res) => {
+//   res.send(wildSeries);
+// });
+const wildSeries = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+app.get("/", wildSeries);
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
