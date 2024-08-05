04e3c0cd926696524c9769aa132b92064f16d898
anaisdufourneau <anais.dufourneau87@gmail.com>
2024-08-05 16:14:46 +0200

    harmonia-4 done !

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,18 +2,28 @@ const express = require("express");
 
 const router = express.Router();
 
-const wildSeries = (req, res) => {
-  res.send("Welcome to Wild Series");
-};
+// const wildRouter = require("./wild/router");
+
+// router.use("/wild", wildRouter);
+
+const { wildSeries } = require("../../controllers/seriesAction");
+
 router.get("/", wildSeries);
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
 
+const categoryRouter = require("./categories/router");
+
+router.use("/categories", categoryRouter);
 /* ************************************************************************* */
 
 module.exports = router;
